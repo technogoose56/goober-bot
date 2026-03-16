c3ad51a75be66f1e6c3579c48315757f4d9f2b4e
Cameron Loren <camm.loren@yahoo.com>
2026-03-16 18:02:15 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,31 @@
+Goober Bot - Telegram Bot
+
+A simple Telegram bot built with Go that responds to greeting messages.
+
+## Setup
+
+1. Get a bot token from [@BotFather](https://t.me/BotFather) on Telegram
+2. Set your token as an environment variable:
+   ```
+   $env:TELEGRAM_BOT_TOKEN="your_bot_token_here"
+   `
+   or
+   ```
+   ```bash
+   export TELEGRAM_BOT_TOKEN="your_bot_token_here"
+   ```
+3. Run the bot:
+   ```bash
+   go run main.go
+   ```
+
+## Features
+
+- Responds to "hi", "Hi", "/hi", and various greetings in different languages
+- Simply displays "Hi, I'm Goober Bot!"
+
+## Usage
+
+1. Add the bot to your Telegram chat
+2. Send "hi" or any of the supported greetings
+3. The bot will automatically respond
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	botToken := os.Getenv("TELEGRAM_BOT_TOKEN")
+	if botToken == "" {
+		log.Fatal("TELEGRAM_BOT_TOKEN environment variable is not set")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(botToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		text := update.Message.Text
+		switch text {
+		case "/hi", "hi", "Hi", "Hola", "Bonjour", "Ciao":
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hi, I'm Goober Bot!")
+			bot.Send(msg)
+		}
+	}
+}
